66a548e5965253348142fbf00c494c41a9ee542f
Jenia Dysin <genia90@gmail.com>
2025-12-21 14:16:26 +0000

    Fix merge issues

--- keyboards/yushakobo/ergo68/keymaps/jedy_via/keymap.c
+++ keyboards/yushakobo/ergo68/keymaps/jedy_via/keymap.c
@@ -10,49 +10,28 @@
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         [0] = LAYOUT(
-<<<<<<< HEAD
             KC_ESC,     KC_1,       KC_2,       KC_3,       KC_4,       KC_5,                               KC_6,           KC_7,       KC_8,       KC_9,           KC_0,       KC_PIPE,
             KC_TAB,     KC_Q,       KC_W,       KC_E,       KC_R,       KC_T,       KC_PGUP,    KC_PSCR,    KC_Y,           KC_U,       KC_I,       KC_O,           KC_P,       KC_BSLS,
             MO(1),      KC_A,       KC_S,       KC_D,       KC_F,       KC_G,       KC_PGDN,    KC_APP,     KC_H,           KC_J,       KC_K,       KC_L,           KC_SCLN,    KC_QUOT,
             KC_LSFT,    KC_Z,       KC_X,       KC_C,       KC_V,       KC_B,       KC_GRV,     KC_DEL,     KC_N,           KC_M,       KC_COMM,    KC_DOT,         KC_SLSH,    KC_RSFT,
-=======
-            KC_ESC,     KC_1,       KC_2,       KC_3,       KC_4,       KC_5,                               KC_6,           KC_7,       KC_8,       KC_9,           KC_0,       KC_PIPE, 
-            KC_TAB,     KC_Q,       KC_W,       KC_E,       KC_R,       KC_T,       KC_PGUP,    KC_PSCR,    KC_Y,           KC_U,       KC_I,       KC_O,           KC_P,       KC_BSLS, 
-            MO(1),      KC_A,       KC_S,       KC_D,       KC_F,       KC_G,       KC_PGDN,    KC_APP,     KC_H,           KC_J,       KC_K,       KC_L,           KC_SCLN,    KC_QUOT, 
-            KC_LSFT,    KC_Z,       KC_X,       KC_C,       KC_V,       KC_B,       KC_GRV,     KC_DEL,     KC_N,           KC_M,       KC_COMM,    KC_DOT,         KC_SLSH,    KC_RSFT, 
->>>>>>> 2351ed404a (Initial commit wth new keymap)
             KC_LALT,    KC_MINS,    KC_EQL,     KC_LCBR,    KC_RCBR,    KC_LCTL,    KC_BSPC,    KC_ENT,     KC_SPC,         KC_LPRN,    KC_RPRN,    KC_LBRC,        KC_RBRC,    KC_RGUI
         ),
         [1] = LAYOUT(
             KC_F12,     KC_F1,      KC_F2,      KC_F3,      KC_F4,      KC_F5,                              KC_F6,          KC_F7,      KC_F8,      KC_F9,          KC_F10,     KC_F11,
-<<<<<<< HEAD
             KC_TRNS,    UG_VALU,    UG_VALD,    UG_SPDU,    UG_SPDD,    KC_TRNS,    UG_NEXT,    KC_TRNS,    LALT(KC_LEFT),  KC_HOME,    KC_END,     LALT(KC_RGHT),  KC_TRNS,    KC_TRNS,
             KC_TRNS,    KC_CAPS,    UG_HUEU,    UG_HUED,    KC_TRNS,    KC_TRNS,    UG_PREV,   KC_TRNS,    KC_LEFT,        KC_DOWN,    KC_UP,      KC_RGHT,        KC_TRNS,    KC_TRNS,
             KC_TRNS,    KC_TRNS,    UG_SATU,    UG_SATD,    KC_TRNS,    KC_TRNS,    UG_TOGG,    KC_INS,     KC_TRNS,        KC_TRNS,    KC_TRNS,    KC_TRNS,        KC_TRNS,    CW_TOGG,
             KC_TRNS,    KC_TRNS,    KC_MPLY,    KC_MPRV,    KC_MNXT,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,        KC_VOLD,    KC_VOLU,    KC_MUTE,        KC_TRNS,    KC_TRNS
-=======
-            KC_TRNS,    RGB_VAI,    RGB_VAD,    RGB_SPI,    RGB_SPD,    KC_TRNS,    RGB_MOD,    KC_TRNS,    LALT(KC_LEFT),  KC_HOME,    KC_END,     LALT(KC_RGHT),  KC_TRNS,    KC_TRNS, 
-            KC_TRNS,    KC_CAPS,    RGB_HUI,    RGB_HUD,    KC_TRNS,    KC_TRNS,    RGB_RMOD,   RGB_VAD,    KC_LEFT,        KC_DOWN,    KC_UP,      KC_RGHT,        KC_TRNS,    KC_TRNS,
-            KC_TRNS,    KC_TRNS,    RGB_SAI,    RGB_SAD,    KC_TRNS,    KC_TRNS,    RGB_TOG,    KC_INS,     KC_TRNS,        KC_TRNS,    KC_TRNS,    KC_TRNS,        KC_TRNS,    CW_TOGG,    
-            KC_TRNS,    KC_TRNS,    KC_MPLY,    KC_MPRV,    KC_MNXT,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,        KC_VOLU,    KC_VOLD,    KC_MUTE,        KC_TRNS,    KC_TRNS
->>>>>>> 2351ed404a (Initial commit wth new keymap)
         )
 };
 
 bool rgb_matrix_indicators_advanced_user(uint8_t led_min, uint8_t led_max) {
     // caps lock cyan
-<<<<<<< HEAD
     if (is_caps_word_on() || host_keyboard_led_state().caps_lock) {
         RGB_MATRIX_INDICATOR_SET_COLOR(0, 0, 0, 128);
         RGB_MATRIX_INDICATOR_SET_COLOR(9, 100, 100, 100);
     } else {
         RGB_MATRIX_INDICATOR_SET_COLOR(0, 0, 0, 0)
-=======
-    if (host_keyboard_led_state().caps_lock) {
-        RGB_MATRIX_INDICATOR_SET_COLOR(0, 0, 0, 128);
-    } else {
-        RGB_MATRIX_INDICATOR_SET_COLOR(0, 0, 0, 0);
->>>>>>> 2351ed404a (Initial commit wth new keymap)
     }
 
     // num lock cyan
@@ -82,7 +61,6 @@ bool rgb_matrix_indicators_advanced_user(uint8_t led_min, uint8_t led_max) {
             break;
     }
     return false;
-<<<<<<< HEAD
 }
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
@@ -107,6 +85,3 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     }
     return true;
 }
-=======
-}
->>>>>>> 2351ed404a (Initial commit wth new keymap)
